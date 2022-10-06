116d20a59c0d23fb6b6da1865a465010f0601fb1
Yuan <yshen47@illinois.edu>
2022-10-06 00:03:05 -0500

    sasve

--- modules/generator.py
+++ modules/generator.py
@@ -15,12 +15,16 @@ class Generator(nn.Module):
     def forward(self, rgbd, mask, z):
         return self.spade_network_noenc(rgbd, mask, z)
 
-    def style_encoding(self, encoding, return_mulogvar=False):
+    def style_encoding(self, encoding, return_mulogvar=False, sample=False):
         mu, logvar = self.spade_encoder(encoding)
-        z = self.spade_encoder.reparameterize(mu, logvar)
+        if sample:
+            z = self.spade_encoder.reparameterize(mu, logvar)
+        else:
+            z = mu
         if return_mulogvar:
             return z, mu, logvar
         return z
+
 
 class SpadeNetworkNoEnc(nn.Module):
 
